c6955740cc0434c50ac22a00c2a217422345019d
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-03-30 11:10:32 -0500

    Time: 1 ms (96.81%), Space: 36.5 MB (41.68%) - LeetHub

--- permutation-sequence/permutation-sequence.java
+++ permutation-sequence/permutation-sequence.java
@@ -1,39 +1,35 @@
 class Solution {
-    
-    public int fact(int n){
-        if(n==0||n==1){
-            return 1;
-        }
-        return n * fact(n-1);
-    }
-    
-    public void findKthPermutation(List<Character> v, int k, StringBuilder result){
-        if(v.isEmpty()){
-            return;
-        }
-        
-        int n = v.size();
-        
-        int count = fact(n-1);
-        int selected = (k-1)/count;
-        
-        result.append(v.get(selected));
-        v.remove(selected);
-        k = k - count * selected;
-        
-        findKthPermutation(v, k, result);
-    }
+
     
     public String getPermutation(int n, int k) {
-        List<Character> v = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
        
-        char c = '1';
-        for(int i= 0; i<n; i++){
-            v.add( c);
-            c++;
+       
+        for(int i= 1; i<=n; i++){
+           numbers.add(i);
         }
-       StringBuilder res =    new StringBuilder();
-        findKthPermutation(v, k, res);
+        int[] fact = new int[n+1];
+        
+        fact[0] = 1;
+        int sum= 1;
+        for(int i=1; i<=n; i++){
+            sum *= i;
+            fact[i] = sum;
+        }
+        
+        k--;
+        
+     StringBuilder res = new StringBuilder();
+        
+        
+    for(int i = 1; i <= n; i++){
+        int index = k/fact[n-i];
+        res.append(String.valueOf(numbers.get(index)));
+        numbers.remove(index);
+        k -= index*fact[n-i];
+     }
+        
+      
         return res.toString();
     }
 }
