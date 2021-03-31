980261dc5200d0bbcbffd7cc39ad6cdc540a4080
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-03-31 14:50:56 -0500

    Time: 5 ms (76.12%), Space: 39.2 MB (63.83%) - LeetHub

---
+++ word-break/word-break.java
@@ -0,0 +1,38 @@
+class Solution {
+    
+   
+public boolean wordBreak(String s, List<String> wordDict) {
+        // create the memoization array to save results and avoid repeat computations
+        Boolean[] canBreak = new Boolean[s.length()];
+
+        // convert the list into set for faster lookup
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        // recursion with memoization
+        return helper(s, 0, wordSet, canBreak);
+    }
+
+    private boolean helper(String s, int startIdx, Set<String> wordSet, Boolean[] canBreak) {
+        // in case we've reached the end of string, return true
+        if (startIdx == s.length()) return true;
+        // else if we've already computed on current substring before
+        if (canBreak[startIdx] != null) return canBreak[startIdx]; // auto-unboxing
+
+        boolean res = false;
+        // iterate through all indices after startIdx, explore every possible word
+        for (int i = startIdx + 1; i <= s.length(); i++) {
+            String currWord = s.substring(startIdx, i);
+            // skip if this is not a word in the input dictionary
+            if (!wordSet.contains(currWord)) continue;
+            // recursively call upon the rest of string
+            if (helper(s, i, wordSet, canBreak)) {
+                res = true;
+                break;
+            }
+        }
+        // add result to memo and return the result
+        canBreak[startIdx] = res;
+        return res;
+    }
+  
+}
