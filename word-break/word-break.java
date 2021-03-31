835acc4e62306c6494ab9a20ddd56b8dd68a33ec
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-03-31 17:33:11 -0500

    Time: 6 ms (62.19%), Space: 39 MB (73.27%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,38 +1,27 @@
 class Solution {
     
-   
+    Set<String> visited = new HashSet();
 public boolean wordBreak(String s, List<String> wordDict) {
-        // create the memoization array to save results and avoid repeat computations
-        Boolean[] canBreak = new Boolean[s.length()];
 
-        // convert the list into set for faster lookup
-        Set<String> wordSet = new HashSet<>(wordDict);
-
-        // recursion with memoization
-        return helper(s, 0, wordSet, canBreak);
+    if(visited.contains(s)){
+        return false;
     }
-
-    private boolean helper(String s, int startIdx, Set<String> wordSet, Boolean[] canBreak) {
-        // in case we've reached the end of string, return true
-        if (startIdx == s.length()) return true;
-        // else if we've already computed on current substring before
-        if (canBreak[startIdx] != null) return canBreak[startIdx]; // auto-unboxing
-
-        boolean res = false;
-        // iterate through all indices after startIdx, explore every possible word
-        for (int i = startIdx + 1; i <= s.length(); i++) {
-            String currWord = s.substring(startIdx, i);
-            // skip if this is not a word in the input dictionary
-            if (!wordSet.contains(currWord)) continue;
-            // recursively call upon the rest of string
-            if (helper(s, i, wordSet, canBreak)) {
-                res = true;
-                break;
-            }
+    
+    for (int i = 1; i <= s.length(); ++i) {
+      String first = s.substring(0, i);
+      if (wordDict.contains(first)) {
+        String second = s.substring(i);
+     
+        if (second == null || second.length() == 0 || wordDict.contains(second) || wordBreak(second, wordDict)) {
+          return true;
+        }else{
+               visited.add(second);
         }
-        // add result to memo and return the result
-        canBreak[startIdx] = res;
-        return res;
+
+      }
     }
+    visited.add(s);
+    return false;
+}
   
 }
