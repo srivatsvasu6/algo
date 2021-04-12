fe95c0ac3ef50103a76687d10894364873ef53d7
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-04-12 16:28:50 -0500

    Time: 0 ms (100.00%), Space: 39.1 MB (49.42%) - LeetHub

--- balanced-binary-tree/balanced-binary-tree.java
+++ balanced-binary-tree/balanced-binary-tree.java
@@ -15,22 +15,21 @@
  */
 class Solution {
     
-     int depth (TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(depth(root.left), depth (root.right)) + 1;
-    }
-    
-    public boolean isBalanced(TreeNode root) {
-        
-        if(root ==null){
+  public boolean isBalanced(TreeNode root) {
+        if(root == null){
             return true;
         }
-      
-        int left=depth(root.left);
-        int right=depth(root.right);
-        
-        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
-      
+        return helper(root) != -1;
     }
-
+    private int helper(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1){
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
 }
