e6163c6de14eb2b5966f3c4bc785fbe4032de357
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-04-16 13:31:23 -0500

    Time: 0 ms (100.00%), Space: 37.5 MB (85.20%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,41 +1,33 @@
 class Solution {
     
-     private static final Map<Character, char[]> KEYBOARD = new HashMap<>();
-    static {
-        KEYBOARD.put('2', "abc".toCharArray());
-        KEYBOARD.put('3', "def".toCharArray());
-        KEYBOARD.put('4', "ghi".toCharArray());
-        KEYBOARD.put('5', "jkl".toCharArray());
-        KEYBOARD.put('6', "mno".toCharArray());
-        KEYBOARD.put('7', "pqrs".toCharArray());
-        KEYBOARD.put('8', "tuv".toCharArray());
-        KEYBOARD.put('9', "wxyz".toCharArray());
-    }
+
         
     public List<String> letterCombinations(String digits) {
+        
+        String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         List<String> res = new ArrayList<>();
         if(digits !=null && digits.length()> 0){
-          dfs(new StringBuilder(), res, digits.toCharArray());
+          dfs(new StringBuilder(), map, res, digits.toCharArray());
         }
        
         return res;
         
     }
     
-    public void dfs(StringBuilder paths,List<String> res, char[] digits ){
+    public void dfs(StringBuilder paths, String[] map, List<String> res, char[] digits ){
         
         if(paths.length() == digits.length){
             res.add(paths.toString());
             return;
         }
         
-        char ch = digits[paths.length()];
+        int ch = Character.getNumericValue(digits[paths.length()]);
         
-        for(char c : KEYBOARD.get(ch)){
+        for(char c : map[ch].toCharArray()){
             
             paths.append(c);
             
-            dfs(paths, res, digits);
+            dfs(paths,map, res, digits);
             
             paths.deleteCharAt(paths.length()-1);
         }
