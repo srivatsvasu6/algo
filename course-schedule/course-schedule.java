1c69a75220b68de952bc6a25695ea0275aba9734
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-04-17 17:29:11 -0500

    Time: 3 ms (86.13%), Space: 39.5 MB (57.30%) - LeetHub

---
+++ course-schedule/course-schedule.java
@@ -0,0 +1,58 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        
+        if(numCourses<1 || prerequisites==null || prerequisites.length==0){
+            return true;
+        }
+        
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        for(int i=0;i<numCourses; i++){
+            map.put(i, new ArrayList<Integer>());
+        }
+        
+        for(int[] pre : prerequisites){
+            
+            map.get(pre[1]).add(pre[0]);
+            
+        }
+        
+        int[] visited = new int[numCourses+1];
+        
+
+        for(int i=0; i<numCourses; i++){
+            if(! dfs(map, i, visited))
+           {
+                return false;
+           }
+        }
+        return true;
+    }
+    
+    public boolean dfs( Map<Integer, List<Integer>> map, int i, int[] visited){
+        
+        if(visited[i]==1){
+            return true;
+        }
+        
+        if(visited[i]==-1){
+            return false;
+        }
+        
+        visited[i] = -1;
+        
+
+        for(Integer course: map.get(i)){
+            
+            if(! dfs(map, course, visited )){
+                return false;
+            }
+        }
+        
+        visited[i] = 1;
+        
+        return true;
+        
+    }
+}
