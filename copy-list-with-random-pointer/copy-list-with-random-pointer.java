2613ec62d6c40451b72a8031327e7d2760b6ba12
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-04-23 18:28:06 -0500

    Time: 0 ms (100.00%), Space: 38.6 MB (54.17%) - LeetHub

--- copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -20,42 +20,45 @@ class Solution {
      if(head == null) return head;
         
         Node curr = head;
-        Node curOriginal=head;
+
         
         while(curr.next!=null){
-         System.out.print(curr.val+"->");
-            Node copy=new Node(curr.val);
-            Node temp=curr.next;
-            curr.next=copy;
-            copy.next=temp;
-            curr=curr.next.next;
+
+            Node copy =new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = curr.next.next;
            
         }
-        
-        curr.next=new Node(curr.val);
+        curr.next = new Node(curr.val);
        
-           //step 2
-    while(curOriginal!= null && curOriginal.next != null){
-        if(curOriginal.random != null){
-            curOriginal.next.random=curOriginal.random.next;
+        curr = head;
+    
+        
+        while( curr!=null ){
+            if(curr.random != null){
+                curr.next.random=curr.random.next;
+            }
+            curr = curr.next.next;
         }
-        curOriginal=curOriginal.next.next;
-    }
+     
+        curr = head;
+        Node res = head.next;
+        Node copy = head.next;
         
-        
-    Node orig=head;
-    Node copyCur=head.next;
-    Node copyHead=head.next;
+        while(copy.next!=null && curr.next!=null){
+           
+            curr.next = curr.next.next;
+            copy.next = copy.next.next;
+            
+            curr = curr.next;
+            copy = copy.next;
+        }
+      
+       
+        curr.next=null;
     
-    while(orig.next != null && copyCur.next != null){
-        orig.next=orig.next.next;
-        copyCur.next=copyCur.next.next;
-        orig=orig.next;
-        copyCur=copyCur.next;
-    }
-    orig.next=null;
-    
-    return copyHead;
+        return res;
         
         
     }
