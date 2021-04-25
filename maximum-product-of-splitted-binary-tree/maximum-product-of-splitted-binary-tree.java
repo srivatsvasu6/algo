53569cd8b59712f1d61532fd09c9f9075a0c0c98
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-04-24 22:19:26 -0500

    Time: 6 ms (96.10%), Space: 63 MB (15.60%) - LeetHub

--- maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -1,85 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int max = 1000000007;
-    
-    long maxSum = Long.MIN_VALUE;
- 
-    
+    long max = 0;
+    int half = 0;
     public int maxProduct(TreeNode root) {
-    
-
-         subTreeSum(root);
-        
-        
-      return (int)(maxSum % max);
-        
+        add(root);
+        half = root.val / 2;
+        compute(root, root.val);
+        return (int) (max % (1e9 + 7));
     }
     
-    public long subTreeSum(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-     
-        
-         Map<TreeNode, Long> memo = new HashMap<>();
-           long  total = totalSum(root, memo);
-        Deque<TreeNode> deq = new LinkedList<>();
-        deq.push(root);
-              
-      
-        while(!deq.isEmpty()){
-            
-            TreeNode node = deq.pop();
-               
-            long sum = node.val;
-                if(node.left!=null){
-                    sum+= totalSum(node.left, memo);
-                    deq.push(node.left);
-                }
-        
-                if(node.right!=null){
-                    sum+= totalSum(node.right, memo);
-                    deq.push(node.right);
-                }
-             
-              maxSum = Math.max(maxSum, (total - sum)  * sum ) ;
-        }
-        
-        
-      
-        
-        return maxSum;
-        
+    public int add(TreeNode root){
+        if (root == null) return 0;
+        root.val += add(root.left) + add(root.right);
+        return root.val;
     }
     
-      public long totalSum(TreeNode root, Map<TreeNode, Long> memo){
-        if(root ==null){
-            return 0;
+    public void compute(TreeNode node, long sum){
+        if (node == null) return;
+        max = Math.max(max, (node.val * (sum - node.val)));
+        if (node.val >= half){
+             compute(node.left, sum);
+             compute(node.right, sum);
         }
-        
-      if(memo.containsKey(root)){
-         return memo.get(root);
-      }
-
-        long res = root.val + totalSum(root.left, memo) + totalSum(root.right, memo); 
-          
-         memo.put( root, res);
-         return res; 
+       
     }
-    
    
 }
