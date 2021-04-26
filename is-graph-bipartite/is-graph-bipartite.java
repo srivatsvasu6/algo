524cc85b8d75d1e5242929995daf6c09ce359fed
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-04-26 15:16:47 -0500

    Time: 0 ms (100.00%), Space: 39.3 MB (59.00%) - LeetHub

--- is-graph-bipartite/is-graph-bipartite.java
+++ is-graph-bipartite/is-graph-bipartite.java
@@ -1,54 +1,39 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         
-        if(graph == null || graph.length ==0){
+        if(graph==null || graph.length==0){
             return true;
         }
-        int size = graph.length;
-        Map<Integer, List<Integer>> map = new HashMap<>();
         
-        for(int i=0; i<size; i++){
-            map.put(i, new ArrayList<>());
-        }
+        int[] visited = new int[ graph.length ];
         
-    
-      
-        for(int i=0; i<graph.length; i++){
-            
-            for(int j=0; j< graph[i].length; j++){
-                
-                  map.get(i).add(graph[i][j]);
-            }
-        }
-        
-        int[] visited = new int[size];
-        
-    
-        for(int i=0; i<size; i++){
-            
-            if(visited[i] == 0 && !dfs(map, i, visited, 1)){
-                return false;
+        for(int i=0; i< graph.length; i++){
+            if(visited[i]==0  && !dfs(graph, visited, i,  1  )){
+                 return false;
             }
             
         }
         
-        return true; 
+        return true;
     }
     
-    public boolean dfs( Map<Integer, List<Integer>> map, int i, int[] visited, int group){
-    
+    public boolean dfs(int[][]graph, int[] visited , int i, int grp){
         
-        visited[i] = group;
+        visited[i] = grp;
         
-        for(Integer path: map.get(i) ){
-            if(visited[path]== group){
+        
+        for(int node : graph[i]){
+            
+            if(visited[node] == grp){
                 return false;
             }
-              if(visited[path]==0 && !dfs(map, path, visited, -group)) {
+            
+            if(visited[node]==0 && !dfs(graph, visited, node, -grp)) {
                 return false;
             }
         }
         
         return true;
+        
     }
 }
