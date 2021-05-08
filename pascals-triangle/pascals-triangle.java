cf74f28fba94f9813a94deaf79240dd153d752c1
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-05-08 17:35:40 -0500

    Time: 0 ms (100.00%), Space: 36.9 MB (35.30%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+  
+        List<Integer> row, pre = null;
+        
+        
+        for(int i=0; i<n; i++){
+           
+           row = new ArrayList<>(); 
+            for(int j=0; j<=i; j++)
+              {   
+                if(j==0 || j==i)
+                    row.add(1);
+                else
+                    row.add(pre.get(j) + pre.get(j-1));
+              }
+            pre = row;
+            res.add(row);
+            
+        
+            }
+        
+        return res;
+    }
+}
