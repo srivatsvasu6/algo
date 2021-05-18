dc19c8d819f1d2160b759ae07ccc838659ed3560
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-05-18 11:23:18 -0500

    Time: 1 ms (99.33%), Space: 38.7 MB (99.56%) - LeetHub

--- subsets-ii/subsets-ii.java
+++ subsets-ii/subsets-ii.java
@@ -6,28 +6,17 @@ class Solution {
             return res;
         }
         Arrays.sort(nums);
-        res.add(new ArrayList<>());
-        
-        int start =0, size = 0;
-        
-        for(int k =0; k< nums.length; k++ ){            
-        
-            start = (k > 0 && nums[k] == nums[k-1])? size: 0; 
-            size = res.size();
-            for(int i=start; i<size; i++){
-               
-                
-                
-                List<Integer> tmp = new ArrayList(res.get(i));
-              
-                tmp.add(nums[k]);
-                
-                res.add(new ArrayList<>(tmp));
-               
-            }
-            
-        }
-        
+       backtrack(res,new ArrayList<>(), nums, 0);
         return res;
     }
+    
+  private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+         if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
+    }
+}
 }
