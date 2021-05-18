209607d56a4501426774237b8cd207ae399fee19
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-05-18 09:09:29 -0500

    Time: 1 ms (99.33%), Space: 38.9 MB (94.22%) - LeetHub

--- subsets-ii/subsets-ii.java
+++ subsets-ii/subsets-ii.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         
-        
-        if(nums==null || nums.length ==0)
-            return new ArrayList<>();
-        
-        
+        if(nums==null || nums.length ==0){
+            return res;
+        }
         Arrays.sort(nums);
-       List<List<Integer>> res = new ArrayList<>(); 
         res.add(new ArrayList<>());
-        int start =0, size=0;
-        for(int i=0; i<nums.length;i++){
-     
-                start = (i> 0 && nums[i] == nums[i-1]) ?size:0;
-                size=  res.size();
-                for(int j =start; j< size; j++){
-                    
-                    List<Integer> sub = new ArrayList<>(res.get(j));
-                      
-                    sub.add(nums[i]);
-                    res.add(new ArrayList<>(sub));
-                }
+        
+        int start =0, size = 0;
+        
+        for(int k =0; k< nums.length; k++ ){            
+        
+            start = (k > 0 && nums[k] == nums[k-1])? size: 0; 
+            size = res.size();
+            for(int i=start; i<size; i++){
+               
                 
-            
+                
+                List<Integer> tmp = new ArrayList(res.get(i));
+              
+                tmp.add(nums[k]);
+                
+                res.add(new ArrayList<>(tmp));
+               
+            }
             
         }
         
