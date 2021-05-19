7b17c6319d4a0826a4a85ff2848112e48da22a25
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-05-19 15:13:00 -0500

    Time: 11 ms (32.89%), Space: 39 MB (37.89%) - LeetHub

--- palindromic-substrings/palindromic-substrings.java
+++ palindromic-substrings/palindromic-substrings.java
@@ -7,14 +7,35 @@ class Solution {
          
            return 0;
         }
+        int cnt = 0;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+         for(int i=0; i< n; i++){
+              dp[i][i] = true;
+             cnt++;
+         }
         
-        for(int i = 0; i<s.length(); i++){
-         
-        isPalindrome(s, i, i);
-        isPalindrome(s, i, i+1);
+        for(int i=0; i< n-1; i++){
+        
+          dp[i][i+1] = s.charAt(i) == s.charAt(i+1);
+            if( dp[i][i+1]){
+                cnt++;
+            }
         }
         
-        return res;
+        for(int len=3; len<=n; len++){
+            
+            for(int i=0, j=i+len-1; j<n; i++,j++){
+                dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i+1][j-1];
+             if( dp[i][j]){
+                cnt++;
+            }
+            }
+        }
+        
+        
+        return cnt;
     }
     
     public void isPalindrome(String s, int l, int r){
