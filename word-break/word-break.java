c6c5c4fd3115c1b1d4abdaf972d5c724006699d9
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-05-20 09:06:16 -0500

    Time: 1 ms (98.94%), Space: 39.1 MB (58.52%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,28 +1,35 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return dfs( 0, new Boolean[s.length()], s, wordDict);
+        
+      return dfs(s,0, new Boolean[s.length()], wordDict);
+        
     }
     
-    public boolean dfs(int i, Boolean[] memo, String s, List<String> wordDict)         {
+    public boolean dfs(String s, int i, Boolean[] memo, List<String> wordDict){
         
-      if(s.length()==i){
-          return true;
-      }
+        if(s==null || s.length()==0 || wordDict==null || wordDict.size()==0 || i==s.length()){
+            
+            return true;
+        }
+           
+     
         if(memo[i]!=null){
             return memo[i];
         }
+        
          boolean found = false;
         
-        for(String word: wordDict ){
-           
-           if(s.substring(i).startsWith(word)){
-               found = found || dfs(i+ word.length(), memo,s,wordDict);
-           }
-        }
+        for(String word : wordDict){
+            
+            if(s.substring(i).startsWith(word)){
+                    found = found || dfs(s, i+ word.length(), memo,wordDict);
+                }
+                
+            }
+        
+    
         memo[i] = found;
         
         return found;
-        
-        
     }
 }
