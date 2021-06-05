91795a01ca37dd1c3c45c8d58b416401c058791d
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-05 17:24:41 -0500

    Time: 1 ms (97.82%), Space: 39 MB (78.04%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,22 +1,49 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> a-b);
-        
-      for(int num: nums){
-          
-          pq.add(num);
-      }
-          
-        for(int i=k; i<nums.length; i++){ 
-            
-           
-                pq.poll();
-             
-            
-        }
-       
-        return pq.peek();
-        
+        k = nums.length - k+ 1;
+      
+        return findKthSmallestNumberRec(nums, k, 0, nums.length - 1);
+    }
+    
+    
+  public  int findKthSmallestNumberRec(int[] nums, int k, int start, int end) {
+    int p = partition(nums, start, end);
+
+    if (p == k - 1)
+      return nums[p];
+
+    if (p > k - 1) // search lower part
+      return findKthSmallestNumberRec(nums, k, start, p - 1);
+
+    // search higher part
+    return findKthSmallestNumberRec(nums, k, p + 1, end);
+  }
+    
+private  int partition(int[] nums, int low, int high) {
+    if (low == high)
+      return low;
+
+    Random randomNum = new Random();
+    int pivotIndex = low + randomNum.nextInt(high - low);
+    swap(nums, pivotIndex, high);
+
+    int pivot = nums[high];
+    for (int i = low; i < high; i++) {
+      // all elements less than 'pivot' will be before the index 'low'
+      if (nums[i] < pivot)
+        swap(nums, low++, i);
+    }
+    // put the pivot in its correct place
+    swap(nums, low, high);
+    return low;
+  }
+
+ 
+
+    public  void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
