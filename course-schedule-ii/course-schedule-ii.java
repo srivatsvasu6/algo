1ed7979a8b0f2831831c81b69620a153da2d7025
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-05 12:04:49 -0500

    Time: 4 ms (79.56%), Space: 39.9 MB (67.91%) - LeetHub

--- course-schedule-ii/course-schedule-ii.java
+++ course-schedule-ii/course-schedule-ii.java
@@ -1,66 +1,46 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-         Map<Integer, List<Integer>> map = new HashMap<>();
-        
-         for(int i=0; i<numCourses; i++){
-            map.put(i, new ArrayList<>());
-         }
         
         
-        for(int[] pre: prerequisites){
-         
-            map.get(pre[0]).add(pre[1]);
-            
-           
-        }
-        
-        
-        List<Integer> stack = new ArrayList<>();
-        int[] visited = new int[numCourses];
-      
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        int cnt =0;
+        int[] res = new int[numCourses];
+        int[] indeg = new int[numCourses];
         
         for(int i=0; i<numCourses; i++){
-            if(visited[i]==0 && !dfs(i, map, stack, visited)){
-                return new int[0]; 
-            }
-
+            map.put(i, new ArrayList<>());
         }
         
-        int[] res = new int[stack.size()];
-        int i = numCourses;
-        for(int n: stack)
-            res[--i] = n;
+        for(int[] pre: prerequisites){
+            map.get(pre[1]).add(pre[0]);
+            indeg[pre[0]]++;
+        }
         
-        return res;
+        Queue<Integer> queue = new LinkedList<>();
         
+        for(int i=0; i<numCourses; i++){
+            
+            if(indeg[i]==0)
+                queue.offer(i);
+        }
         
-    }
-    
-    public boolean dfs( int node,  Map<Integer, List<Integer>> map ,  List<Integer> stack,    int[] visited){
-       if(visited[node]==1)
-           return true;
-       else if(visited[node]==-1)
-           return false;
-        visited[node] = -1;
-        
-        if(!map.containsKey(node))
-            return true;
-        
-        for(Integer adj : map.get(node)){
-  
-                if(!dfs(adj, map, stack, visited)){
-                    return false;
-                }
+        while(! queue.isEmpty()){
+            int node = queue.poll();
+            res[cnt++] = node;
+            
+            for(int adj: map.get(node)){
+                
+                indeg[adj]--;
+                if(indeg[adj]==0)
+                    queue.offer(adj);
+            }
             
         }
-        visited[node] = 1;
         
-         stack.add(0, node);
-          
+        if(cnt != numCourses){
+            return new int[0];
+        }
         
-       return true;
-        
+        return res;
     }
-    
-  
 }
