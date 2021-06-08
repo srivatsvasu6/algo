41ea0501a027f55e66da4a4a17a2a2ec80071cb7
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-08 15:46:28 -0500

    Time: 35 ms (5.90%), Space: 45.8 MB (5.94%) - LeetHub

--- fizz-buzz/fizz-buzz.java
+++ fizz-buzz/fizz-buzz.java
@@ -1,17 +1,22 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        
+        String s= "";
         List<String> res = new ArrayList();
         for(int i=1; i<=n; i++){
+            s= "";
+      
             
-          if(i%3 ==0 && i%5 == 0)
-              res.add("FizzBuzz");
-            else if(i%3 ==0 )
-              res.add("Fizz");
-            else if( i%5 == 0)
-              res.add("Buzz");
-            else
-                res.add(String.valueOf(i));
+            if(i%3 ==0 )
+             s+= "Fizz";
+            
+            if( i%5 == 0)
+               s+=  "Buzz";
+            
+            
+             if(i%3 !=0 && i%5 != 0)
+             s+=i;
+                 
+            res.add(s);
         }
         
         return res;
