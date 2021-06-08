75772c20d1e31e9e9e84be9ec18bdb1204a97c7f
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-07 19:49:41 -0500

    Time: 5 ms (45.02%), Space: 37.3 MB (75.32%) - LeetHub

---
+++ interleaving-string/interleaving-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+       
+        
+        int m = s1.length();
+        int n = s2.length();
+        
+        if(m+n != s3.length()){
+            return false;
+        }
+        
+        boolean[][] dp = new boolean[m+1][n+1];
+        
+        dp[0][0]= true;
+        
+        char[] A = s1.toCharArray();
+        char[] B = s2.toCharArray();
+        char[] C = s3.toCharArray();
+        
+       
+        
+        for(int i=0; i<=m; i++){
+            
+            for(int j=0; j<=n; j++){
+                if (i==0 && j==0)
+                     dp[i][j] = true;
+                else if (i == 0) 
+                     dp[i][j] = dp[i][j - 1] && B[j - 1] == C[i + j - 1];
+                else if (j == 0) 
+                     dp[i][j] = dp[i-1][j] && A[i - 1] == C[i + j - 1];
+                
+                else 
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
