04a83ce55ae3008d1888c9f9b7bb44d8cd1efdd5
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-08 20:21:39 -0500

    Time: 2 ms (24.15%), Space: 37.9 MB (12.63%) - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,18 +1,32 @@
 class Solution {
-    public int mySqrt(int n) {
-        if(n==0) return 0;
-        int left = 1;
-        int right = n;
-        int boundaryIndex = -1;
-        while (left <= right) {
-            int mid = left + (right-left)  / 2;
-             if (mid   <= n/mid) {
-                boundaryIndex = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+    public int mySqrt(int x) {
+        if(x<2) 
+          return x;
+        
+        int i = 0, j = x/2;
+        
+        int mid;
+        long num;
+        
+        
+        while(i <= j){
+            
+            mid = i + (j-i)/2;
+          
+            num = (long) mid * mid;
+            
+            if(num > x ){
+                j = mid - 1;
             }
+            else if(num < x){
+                i = mid +1;
+            }else{
+                return mid;
+            }
+            
         }
-        return boundaryIndex;
+        
+        return j;
+        
     }
 }
