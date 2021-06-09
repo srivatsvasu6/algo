259d4e09e7ed030fe2d68638fd82db2ce19c2473
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-08 20:13:03 -0500

    Time: 4 ms (28.08%), Space: 39.8 MB (8.79%) - LeetHub

--- add-strings/add-strings.java
+++ add-strings/add-strings.java
@@ -1,28 +1,28 @@
 class Solution {
     public String addStrings(String num1, String num2) {
         
-  StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         int m = num1.length() -1;
+        
         int n = num2.length() -1;
-   
-        int s=0, c=0;
         
-        while(m>=0 || n>= 0){
+        int carry = 0;
+        
+        while(m>=0 || n>=0){
             
-          int x1 = m>=0? num1.charAt(m) -'0' :0;
-          int x2 = n>=0? num2.charAt(n) -'0' :0;
+            int a = m>=0? num1.charAt(m) -'0' :0;
+            int b = n>=0? num2.charAt(n) -'0' :0;
             
-            s = (x1+x2+c)%10;
-            c = (x1+x2+c)/10;
+            int val = a + b + carry;
+            carry = val /10;
             
-            sb.append(s);
-            m--;n--;
-            
+            sb.append(val % 10);
+            m--;
+            n--;
         }
         
-        if(c!=0){
-            sb.append(c);
-        }
+        if(carry!=0)
+            sb.append(carry);
         
         return sb.reverse().toString();
     }
