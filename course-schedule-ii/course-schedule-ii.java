a606b66f0fff2b6f217066aba611a1995a5dfdf2
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-10 09:18:58 -0500

    Time: 4 ms (80.72%), Space: 39.4 MB (98.81%) - LeetHub

--- course-schedule-ii/course-schedule-ii.java
+++ course-schedule-ii/course-schedule-ii.java
@@ -1,46 +1,48 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         
+
+        
+        int[] indeg = new int[numCourses];
         
         Map<Integer,List<Integer>> map = new HashMap<>();
-        int cnt =0;
-        int[] res = new int[numCourses];
-        int[] indeg = new int[numCourses];
         
         for(int i=0; i<numCourses; i++){
             map.put(i, new ArrayList<>());
         }
         
         for(int[] pre: prerequisites){
+            
             map.get(pre[1]).add(pre[0]);
             indeg[pre[0]]++;
         }
-        
-        Queue<Integer> queue = new LinkedList<>();
-        
+          Queue<Integer> q = new LinkedList<>();
+           
         for(int i=0; i<numCourses; i++){
-            
-            if(indeg[i]==0)
-                queue.offer(i);
+         if(indeg[i]==0)
+             q.add(i);
         }
+      
+        int[] res = new int[numCourses];
         
-        while(! queue.isEmpty()){
-            int node = queue.poll();
-            res[cnt++] = node;
-            
-            for(int adj: map.get(node)){
-                
-                indeg[adj]--;
-                if(indeg[adj]==0)
-                    queue.offer(adj);
-            }
-            
-        }
+       int i=0; 
+      while(!q.isEmpty()){
+          
+          int node = q.poll();
+            res[i] = node;
+            i++;
+          
+          for(int adj : map.get(node)){
+              indeg[adj]--;
+              if(indeg[adj] == 0)
+                  q.offer(adj);
+              
+          }
+      }
         
-        if(cnt != numCourses){
-            return new int[0];
-        }
         
-        return res;
+        return i!=numCourses ? new int[0]: res ;
+      
+        
     }
 }
