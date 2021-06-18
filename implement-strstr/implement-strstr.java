ab73e5d2a8b523e00713875a49cbc55b9ef979d5
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-18 16:42:55 -0500

    Time: 2 ms (48.33%), Space: 38 MB (44.88%) - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int j = 0;
+        for(int i=0; i< (haystack.length() - needle.length() + 1 ); ++i){
+      
+            for( j =0; j<needle.length(); ++j){
+                
+                if(haystack.charAt(i+j) !=needle.charAt(j) ){
+                    break;
+                }
+            }
+            
+            if(j == needle.length() ){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
