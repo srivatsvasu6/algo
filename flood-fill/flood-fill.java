c68ec02239eec36eeba8578bf21f9bf94d6322cf
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-24 20:35:59 -0500

    Time: 0 ms (100.00%), Space: 39.7 MB (68.41%) - LeetHub

--- flood-fill/flood-fill.java
+++ flood-fill/flood-fill.java
@@ -1,68 +1,28 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if(image[sr][sc]!= newColor){
-             dfs(image, new Coordinates(sr, sc), newColor);
+    public int[][] floodFill(int[][] grid, int sr, int sc,  int newColor) {
+        int m = grid.length;
+        int n = grid[0].length;
+     
+        int c = grid[sr][sc];
+        if(c !=newColor)
+        dfs(grid,  sr, sc, m, n, c, newColor);
+                   
+        
+        return grid;
+    }
+    
+    public void dfs(int[][] grid,  int i , int j, int m, int n, int c, int nc){
+        
+        if(i<0 || j<0 || i>=m ||j>=n ||  grid[i][j] !=c ){
+            return;
         }
        
-        return image;
         
+         grid[i][j]=nc;
         
-    }
-    
-    public void dfs(int[][] image, Coordinates start,  int newColor ){
-        
-      
-      int numRows = image.length;
-      int numCols= image[0].length;
-      int color = image[start.x][start.y];
-    //  image[start.x][start.y] = newColor;
-      Queue<Coordinates> queue = new LinkedList<>();
-      queue.offer(start);
-        
-      while(!queue.isEmpty())  {
-        
-        Coordinates node = queue.poll();
-        image[node.x][node.y] = newColor; 
-        List<Coordinates>  pixels = getNeighbour(node, numRows, numCols);
-
-        for(Coordinates pixel: pixels){
-            if(image[pixel.x][pixel.y] == color ){
-                image[pixel.x][pixel.y] = newColor;
-                queue.offer(pixel); 
-            }
-         
-           
-        }
-            
-      } 
-    }
-    
-    public  List<Coordinates> getNeighbour(Coordinates start, int numRows, int numCols){
-      List<Coordinates> coordinates = new ArrayList<>();
-  
-        
-         int[] deltaRow = {-1,0,1,0};
-         int[] deltaCol = {0,1,0,-1};
-        
-        for(int i=0; i<deltaRow.length; i++){
-            
-            int nr = deltaRow[i] + start.x;
-            int nc = deltaCol[i] + start.y;
-            
-            if(0<= nr && nr<numRows && 0<= nc && nc< numCols){
-                coordinates.add(new Coordinates(nr, nc));
-            }
-        }
-        
-        return coordinates;
-        
-    }
-    
-    class Coordinates{
-        int x,y;
-        Coordinates(int x, int y){
-            this.x =x;
-            this.y =y;
-        }
+         dfs(grid, i+1, j, m, n, c, nc);
+         dfs(grid, i, j+1, m, n, c, nc);
+         dfs(grid,i-1, j, m, n, c, nc);
+         dfs(grid,i, j-1, m, n, c, nc);
     }
 }
