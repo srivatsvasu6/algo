1257b333e1fdfb581677f9ce7643ba903f0cad68
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-24 22:31:40 -0500

    Time: 301 ms (55.81%), Space: 39.5 MB (54.32%) - LeetHub

--- cut-off-trees-for-golf-event/cut-off-trees-for-golf-event.java
+++ cut-off-trees-for-golf-event/cut-off-trees-for-golf-event.java
@@ -1,8 +1,8 @@
 class Solution {
        
-      int[][] dirs = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-    public int cutOffTree(List<List<Integer>> forest) {
-      
+   public int cutOffTree(List<List<Integer>> forest) {
+        
+        
         if (forest == null || forest.size() == 0) return 0;
         int m = forest.size(), n = forest.get(0).size();
         
@@ -23,7 +23,7 @@ class Solution {
             
             int[] edge = pq.poll();
 
-            int minStep = bfs(forest, start[0], start[1], edge[0], edge[1] );
+            int minStep = getMin(forest, start, edge, m, n );
             
             if(minStep<0)
                 return -1;
@@ -35,47 +35,41 @@ class Solution {
         }
         
         return step;
+        
     }
     
-    private int bfs(List<List<Integer>> forest, int x, int y, int tx, int ty) 
-    {
-        //A typical BFS approach that we use
-        int m = forest.size(), n = forest.get(0).size();
+    private int getMin(List<List<Integer>> forest, int[] start,int[] edge,int m,int n ){
+        int step = 0;
         Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[m][n];
+        int[][] dirs =  {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
+        queue.offer(start);
+        visited[start[0]][start[1]] = true;
         
-        //Add the current source coordinates to the queue and mark it as visited
-        queue.add(new int[]{x, y});
-        visited[x][y] = true;
-        
-        int dist = 0;
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-           
-            for (int j = 0; j < size; j++) {
-                int[] cur = queue.poll();
+        while(!queue.isEmpty()){
+            
+            for(int i= queue.size(); i>0; i--){
                 
-                 //If we have reached the tree at (tx,ty) through traversal then that means we can return the distance covered from (x,y) to (tx,ty)
-                if (cur[0] == tx && cur[1] == ty) 
-                    return dist;
+                int[] node = queue.poll();
                 
-                //Traverse in all 4 directions and then process it if it meets the conditions
-                for (int i = 0; i < 4; i++) 
-                {
-                    int nx = cur[0]+dirs[i][0];
-                    int ny = cur[1]+dirs[i][1];
+                if(node[0]==edge[0] && node[1]==edge[1])
+                    return step;
+                
+                for(int[] dir: dirs){
+                    int dx = dir[0] + node[0];
+                    int dy = dir[1] + node[1];
                     
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && 
-                        !visited[nx][ny] && forest.get(nx).get(ny) >= 1)
-                    {
-                      visited[nx][ny] = true;
-                      queue.add(new int[]{nx, ny});
-                   }
+                    if(dx< 0|| dy<0 ||dx>=m || dy>=n || forest.get(dx).get(dy)==0 || visited[dx][dy])
+                        continue;
+                    
+                    visited[dx][dy]= true;
+                    queue.offer(new int[]{dx,dy });
                 }
             }
-            dist++;
+            step++;
         }
+        
         return -1;
+        
     }
 }
