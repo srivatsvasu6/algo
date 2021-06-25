42bc517c85a674ae178ced028bc38baa775b56d8
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-06-25 13:59:44 -0500

    Time: 3 ms (75.86%), Space: 39.5 MB (37.80%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
-        List<List<Integer>>  ls = new ArrayList<>();
+        List<List<Integer>>  res = new ArrayList<>();
         
-        backtrack(ls,  target,  new ArrayList<>(), candidates, 0 );
-        return ls;
+        backtrack(candidates, 0, target, new LinkedList<>(), res);
+        return res;
     }
     
-    public void backtrack(  List<List<Integer>>  ls , int target,List<Integer> temp, int[] candidates, int start ){
-       if(target < 0) return;
-        else if(target == 0) ls.add(new ArrayList<>(temp)); 
-        else{
+    private void backtrack(int[] candidates, int start, int remain,LinkedList<Integer> comb, List<List<Integer>>  res  ){
         
-        for(int i=start; i<candidates.length; i++){
+        
+        if(remain == 0){
+          res.add(new ArrayList<>(comb));
+            return;
+        }else if(remain < 0){
+            return;
+        } else{
             
-            temp.add(candidates[i]);
-            backtrack(ls, target - candidates[i],  temp, candidates, i);
-            temp.remove(temp.size() -1);
+            for(int i= start; i< candidates.length; i++){
+                
+                comb.add(candidates[i]);
+                backtrack(candidates, i, remain-candidates[i] , comb, res);
+                comb.removeLast();  
+                
+            }
+            
             
         }
-        }
-        
-        
     }
 }
