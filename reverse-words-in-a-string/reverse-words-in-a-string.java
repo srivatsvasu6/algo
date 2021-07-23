b5c2d19e8e4191b03446ddb7f2f769a59c03079b
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-07-22 21:03:09 -0500

    Time: 5 ms (69.95%), Space: 39.3 MB (47.47%) - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.java
+++ reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,43 +1,29 @@
 class Solution {
     public String reverseWords(String s) {
-        
-        
-        s = s.trim();
-        
-       s =  s.replaceAll("[\\s]+"," ");
-        
-        char[] c = s.toCharArray();
-         int i = 0, j = c.length -1;
-        reverse(c, i, j);
-        int prev = 0;
-        for(i=0; i< c.length; i++){
-            
-            if(c[i] ==' '){
-                reverse(c, prev, i-1);
-                prev = i+1;
-            }
-            
-            
-        }
-         reverse(c, prev, c.length-1);
-        return new String(c);
-        
-        
+             int left = 0, right = s.length() - 1;
+    // remove leading spaces
+    while (left <= right && s.charAt(left) == ' ') ++left;
+
+    // remove trailing spaces
+    while (left <= right && s.charAt(right) == ' ') --right;
+
+    Deque<String> d = new ArrayDeque();
+    StringBuilder word = new StringBuilder();
+    // push word by word in front of deque
+    while (left <= right) {
+      char c = s.charAt(left);
+
+      if ((word.length() != 0) && (c == ' ')) {
+        d.offerFirst(word.toString());
+        word.setLength(0);
+      } else if (c != ' ') {
+        word.append(c);
+      }
+      ++left;
     }
-    
-    public void reverse(char[] c, int i, int j){
-      
-        while(i<j){
-            swap(c, i, j);
-            i++;
-            j--;
-        }
-    }
-    
-    public void swap(char[] arr, int i, int j){
+    d.offerFirst(word.toString());
+
         
-        char tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        return String.join(" ", d);
     }
 }
