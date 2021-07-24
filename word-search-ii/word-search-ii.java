8ecf75fdf0511af6d58adde71f8c648a60abaa08
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-07-24 08:33:33 -0500

    Time: 92 ms (71.76%), Space: 39.6 MB (30.46%) - LeetHub

--- word-search-ii/word-search-ii.java
+++ word-search-ii/word-search-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         
-    Trie trie= new Trie();
+    TrieNode trie= buildTrie(words);
         
-        for(String word: words){
-            trie.insert(word);
-        }
-       Set<String>  res = new HashSet<>(); 
+       List<String>  res = new ArrayList<>(); 
     
         
         for(int i = 0; i< board.length; i++){
             for(int j = 0; j< board[i].length; j++){
                 
-             dfs(board, "",trie,i, j, res);
+             dfs(board, i, j, trie, res);
             }
         }
        
@@ -20,121 +17,58 @@ class Solution {
         
     }
     
-    public void dfs(char[][] board, String str,  Trie trie, int i, int j, Set<String>  res){
+    public TrieNode buildTrie(String[] words){
+        TrieNode root =new TrieNode();
         
-        int m = board.length;
-        int n = board[0].length;
-        
-        if(i<0 || j<0|| i>=m || j>=n || board[i][j]=='#')
-            return;
-       
-        char ch = board[i][j];
-        
-        
-        str += ch;
-        
-        if (!trie.startsWith(str)) return;
-        
-        if (trie.search(str)) {
-            res.add(str);
+        for(String word: words){
+            TrieNode p = root;
+            for(char c: word.toCharArray()){
+                
+                int idx = c - 'a';
+                
+                if(p.next[idx]==null){
+                    p.next[idx] = new TrieNode();
+                  
+                }
+                  p = p.next[idx];
+            }
+            p.word = word;
+            
+            
         }
-        
-        board[i][j] = '#';
-        dfs(board, str,trie, i - 1, j, res);
-        dfs(board, str,trie, i + 1, j, res);
-        dfs(board, str,trie, i, j - 1, res);
-        dfs(board, str,trie, i, j + 1, res);
-        board[i][j] = ch;
-        
+        return root;
     }
+    
+    public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+    char c = board[i][j];
+    if (c == '#' || p.next[c - 'a'] == null) return;
+    p = p.next[c - 'a'];
+    if (p.word != null) {   // found one
+        res.add(p.word);
+        p.word = null;     // de-duplicate
+    }
+
+    board[i][j] = '#';
+    if (i > 0) dfs(board, i - 1, j ,p, res); 
+    if (j > 0) dfs(board, i, j - 1, p, res);
+    if (i < board.length - 1) dfs(board, i + 1, j, p, res); 
+    if (j < board[0].length - 1) dfs(board, i, j + 1, p, res); 
+    board[i][j] = c;
+}
     
     
     
 }
 
-class Trie{
-    
-    public TrieNode root;
-    
-    public Trie(){
-        root = new TrieNode();
-        
-    }
-    
-    public void insert(String word){
-        TrieNode node = root;
-        for(int i =0; i<word.length(); i++){
-            char ch = word.charAt(i);
-            if(!node.containsKey(ch)){
-                node.put(ch, new TrieNode());
-            }
-            node = node.get(ch);
-            
-        }
-        node.setEnd();
-        
-    }
-    
-    public boolean search(String word) {
-       TrieNode node = searchPrefix(word);
-      
-        return node != null && node.isEnd();
-        
-    }
-    
-    public boolean startsWith(String word){
-        TrieNode node = searchPrefix(word);
-      
-        return node != null;
-    }
-    
-    public TrieNode searchPrefix(String word){
-        
-        TrieNode node = root;
-        
-        for(int i =0; i<word.length(); i++ ){
-            
-            char ch = word.charAt(i);
-            
-            if(node.containsKey(ch)){
-                node = node.get(ch);
-            }else{
-                return null;
-            }
-            
-        }
-        return node;
-    }
-    
-    
-}
+
 
 class TrieNode{
     
-    private TrieNode[] child;
-    private boolean end;
+     TrieNode[] next;
+     String word; 
     
     public TrieNode(){
-        child = new TrieNode[26];
+        next = new TrieNode[26];
     }
-    
-    public boolean containsKey(char ch){
-        return child[ch-'a'] !=null;
-    }
-    
-    public TrieNode get(char ch){
-        return child[ch -'a'];
-    }
-    
-    public void put(char ch, TrieNode node){
-        child[ch-'a'] = node;
-    }
-    
-    public boolean isEnd(){
-        return end;
-    }
-    
-    public void setEnd(){
-        end = true;
-    }
+  
 }
