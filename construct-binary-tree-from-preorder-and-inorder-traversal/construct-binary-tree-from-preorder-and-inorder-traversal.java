996b4b82693a9a238e567fe33ee9619413bd3fe7
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-07-23 21:55:47 -0500

    Time: 1 ms (98.99%), Space: 39 MB (71.82%) - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,25 +14,32 @@
  * }
  */
 class Solution {
-   public TreeNode buildTree(int[] preorder, int[] inorder ) {
-        return helper( 0 , 0,   inorder.length-1, preorder, inorder);
-    }
-    
-   public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-    if (preStart > preorder.length - 1 || inStart > inEnd) {
-        return null;
-    }
-    TreeNode root = new TreeNode(preorder[preStart]);
-    int inIndex = 0; 
-    for (int i = inStart; i <= inEnd; i++) {
-        if (inorder[i] == root.val) {
-            inIndex = i;
+    int preIndex=0;
+    Map<Integer,Integer> inorderMap;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preIndex=0;
+        inorderMap = new HashMap<>();
+        for(int i=0; i<inorder.length; i++){
+            inorderMap.put(inorder[i], i);
         }
+        
+        return arrToTree(preorder, 0,preorder.length-1 );
     }
-    root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-    root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
-    return root;
-}
-    
+     public TreeNode arrToTree(int[] preorder, int left, int right){
+         
+         if(left > right)
+             return null;
+         
+         int rootVal = preorder[preIndex++];
+         
+         TreeNode root = new TreeNode(rootVal);
+         
+         root.left =  arrToTree(preorder, left , inorderMap.get(rootVal)-1 );
+         
+         root.right = arrToTree(preorder,  inorderMap.get(rootVal)+1, right );
+         
+         return root;
+         
+     }
     
 }
