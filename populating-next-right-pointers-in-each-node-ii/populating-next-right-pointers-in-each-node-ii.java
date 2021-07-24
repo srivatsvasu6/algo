6b66f67b3aa4a2324fe786903f23a58d8ff0373a
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-07-23 19:42:41 -0500

    Time: 0 ms (100.00%), Space: 39.1 MB (23.10%) - LeetHub

--- populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
+++ populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -24,34 +24,39 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         
+        Node curr = root;
+        Node prev = null;
+        Node head = null;
         
-        Node head = root;
-        
-        while(head!=null){
+        while(curr!=null){
             
-            Node dummy = new Node();
-            Node temp = dummy;
-          
-        while(head!=null){ 
-            if (head.left != null){
-               
-                temp.next = head.left;
-                temp = temp.next;
-            
-            } if (head.right != null)
-               {
-                 temp.next = head.right;
-                temp = temp.next;
-             }
-    
-              head = head.next;    
-            
-           }
-           head = dummy.next;
            
+            while(curr!=null){
+                
+                if(curr.left!=null){
+                   if(prev!=null){
+                       prev.next = curr.left;
+                   }else{
+                       head = curr.left;
+                   }
+                  prev = curr.left;   
+                }
+                 if(curr.right!=null){
+                   if(prev!=null){
+                       prev.next = curr.right;
+                   }else{
+                       head = curr.right;
+                   }
+                  prev = curr.right;   
+                }
+                
+                curr = curr.next;
+            }
+       
+            curr = head;
+            head = null;
+            prev = null;
         }
-        
         return root;
-        
     }
 }
