79dbabfcfdfcb81576c01ae25f95e9af51aafb50
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-07-24 12:46:24 -0500

    Time: 2 ms (87.79%), Space: 37.8 MB (86.75%) - LeetHub

--- regular-expression-matching/regular-expression-matching.java
+++ regular-expression-matching/regular-expression-matching.java
@@ -1,32 +1,22 @@
 class Solution {
     public boolean isMatch(String s, String p) {
       
-        if(p.length() ==0) return s.length()==0;
+        if (p == null || p.length() == 0) return (s == null || s.length() == 0);
         
-        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-        
-        dp[s.length()][p.length()] = true;
-        
-        
-         
-             
-        for(int i= s.length(); i>=0; i--){
-            
-            for(int j =p.length()-1; j>=0; j--){
-                
-                
-                boolean first = (i< s.length() && (p.charAt(j)== s.charAt(i)
-                        || p.charAt(j)=='.'));
-                
-                if(j+1 < p.length() && p.charAt(j+1)=='*'){
-                    
-                    dp[i][j] = dp[i][j+2] || first && dp[i+1][j];
-                }else{
-                      dp[i][j] = first && dp[i+1][j+1];
-                }
-            }
+        boolean dp[][] = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int j=2; j<=p.length(); j++) {
+            dp[0][j] = p.charAt(j-1) == '*' && dp[0][j-2]; 
         }
         
-        return dp[0][0];
+        for (int j=1; j<=p.length(); j++) {
+            for (int i=1; i<=s.length(); i++) {
+                if (p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '.') 
+					dp[i][j] = dp[i-1][j-1];
+                else if(p.charAt(j-1) == '*')
+                    dp[i][j] = dp[i][j-2] || ((s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') && dp[i-1][j]); 
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 }
