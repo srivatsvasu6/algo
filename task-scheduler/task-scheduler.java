fe404f9e91b981cd791499d26f48e709053d731c
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-08-10 06:30:32 -0500

    Time: 2 ms (98.73%), Space: 40.7 MB (18.42%) - LeetHub

--- task-scheduler/task-scheduler.java
+++ task-scheduler/task-scheduler.java
@@ -1,53 +1,16 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         
-        if(n==0)
-            return tasks.length;
-        
-        Map<Character, Integer> freq = new HashMap<>();
-        
-        for(char task: tasks){
-            
-            freq.put(task, freq.getOrDefault(task,0)+1);
+        char[] cnt = new char[26];
+        int maxn = 0;
+        for (int task : tasks) {
+            cnt[task - 'A'] ++;
+            maxn = Math.max(maxn, cnt[task - 'A']);
         }
-        
-        PriorityQueue<Map.Entry<Character,Integer>> pq = new PriorityQueue<>((a,b)-> (b.getValue() == a.getValue())? a.getKey()-b.getKey(): b.getValue() - a.getValue());
-        int k = 0;
-        pq.addAll(freq.entrySet());
-       
-       
-        int count = 0;
-        
-        while(!pq.isEmpty()){
-            k = n + 1;
-         List<Map.Entry<Character,Integer>> ls = new ArrayList<>();
-        
-            while( !pq.isEmpty() && k > 0 )
-           { 
-                Map.Entry<Character, Integer> ent = pq.poll();
-                ent.setValue(ent.getValue() - 1);
-                System.out.print(ent.getKey()+"->");
-                if(ent.getValue() > 0){
-                    ls.add(ent);
-                }
-                k--;
-                count++;
-           }
-              pq.addAll(ls);
-        
-            if(pq.isEmpty())
-                break;
-
-            IntStream.of(k).forEach(i-> System.out.print("idle->"));
-            
-            count += k;
-        
-        
-          
-            
-            
-        }
-        
-        return count;
+        int ans = (maxn - 1) * (n + 1);
+        for (int i = 0; i < 26; i ++)
+            if (cnt[i] == maxn)
+                ans ++;
+        return Math.max(ans, tasks.length);
     }
 }
