36275e0df93297c2a327cfd64667cf16e91dc741
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-08-10 23:31:38 -0500

    Time: 1 ms (99.99%), Space: 36.3 MB (20.97%) - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,32 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x<2) 
-          return x;
-        
-        int i = 0, j = x/2;
-        
-        int mid;
-        long num;
-        
-        
-        while(i <= j){
-            
-            mid = i + (j-i)/2;
-          
-            num = (long) mid * mid;
-            
-            if(num > x ){
-                j = mid - 1;
-            }
-            else if(num < x){
-                i = mid +1;
-            }else{
-                return mid;
-            }
-            
-        }
-        
-        return j;
-        
+   if (x < 2) return x;
+
+    long num;
+    int pivot, left = 2, right = x / 2;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      num = (long)pivot * pivot;
+      if (num > x) right = pivot - 1;
+      else if (num < x) left = pivot + 1;
+      else return pivot;
+    }
+
+    return right;
     }
 }
