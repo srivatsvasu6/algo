bfac3f0675dd4a461c5d1058598b640ed4725e0e
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-08-17 21:31:43 -0500

    Time: 0 ms (100.00%), Space: 39.5 MB (14.97%) - LeetHub

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -12,53 +12,35 @@ class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         
         ListNode curr = head;
-         ListNode prev = null;
-        
-        // Head of the final, moified linked list
-        ListNode new_head = null;
-         ListNode ktail = null;
-        
-        
-        while(curr!=null){
-            
         int count = 0;
-            curr = head;
-            
-            while(count< k && curr !=null){
-                
-                curr = curr.next;
-                count++;
-            }
-            
-            if(count ==k){
-                ListNode rev = reverse(head, k);
-                
-                if(new_head ==null)
-                    new_head = rev;
-                
-                if(ktail !=null){
-                    ktail.next = rev;
-                }
-                
-                ktail = head;
-                head = curr;
-            }
-            
+        
+        while(count < k && curr!=null){
+            curr= curr.next;
+            count++;
         }
         
-            // attach the final, possibly un-reversed portion
-        if (ktail != null)
-            ktail.next = head;
         
-        return new_head == null ? head : new_head;
+        if(count == k){
+            
+            ListNode revHead = reverse(head, k);
+            head.next = reverseKGroup(curr, k);
+            return revHead;
+        }
+        
+        return head;
         
     }
     
     public ListNode reverse(ListNode head, int k){
-         ListNode prev = null;
+        
+        ListNode prev = null;
+        
         ListNode curr = head;
-        while(k > 0 && curr!=null){
+        
+        while(k > 0){
+            
             ListNode next = curr.next;
+            
             curr.next = prev;
             prev = curr;
             curr = next;
@@ -66,6 +48,5 @@ class Solution {
         }
         
         return prev;
-        
     }
 }
