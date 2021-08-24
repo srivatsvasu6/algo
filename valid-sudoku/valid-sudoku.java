d54ed2adecf8c053c0b9278c677ad1b98a28d06f
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-08-24 16:56:46 -0500

    Time: 2 ms (73.42%), Space: 38.7 MB (90.31%) - LeetHub

--- valid-sudoku/valid-sudoku.java
+++ valid-sudoku/valid-sudoku.java
@@ -1,48 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-       
-        
-        for(int i=0; i<9; i++){
-            
-            for(int j =0; j<9; j++){
-                
-                if(board[i][j] !='.' && !validSudoku(board, i, j) ){
-                    return false;
-                }
-            }
+    for(int i = 0; i<9; i++){
+        HashSet<Character> rows = new HashSet<Character>();
+        HashSet<Character> columns = new HashSet<Character>();
+        HashSet<Character> cube = new HashSet<Character>();
+        for (int j = 0; j < 9;j++){
+            if(board[i][j]!='.' && !rows.add(board[i][j]))
+                return false;
+            if(board[j][i]!='.' && !columns.add(board[j][i]))
+                return false;
+            int RowIndex = 3*(i/3);
+            int ColIndex = 3*(i%3);
+            if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+                return false;
         }
-        
-        return true;
     }
-    
-
-        
-    public boolean validSudoku(char[][] board, int i, int j){
-        char c = board[i][j];
-        
-        for(int k=0; k<9; k++){
-            
-            if(k!= j && c == board[i][k]){
-                return false;
-            }
-            
-            if(k!= i && c == board[k][j]){
-                return false;
-            }
-            
-            int row = 3*(i/3) + k/3; 
-            int col = 3*(j/3) + k%3;
-            
-         
-            
-            if(row!= i && col !=j && board[row][col]== c){
-                return false;
-            }
-            
-            
-        }
-        return true;
-        
-        
+    return true;
     }
 }
