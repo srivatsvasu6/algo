0199135b78868b9db973450ef7577b3068410cfa
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-08-24 22:12:22 -0500

    Time: 0 ms (100.00%), Space: 36.8 MB (98.81%) - LeetHub

--- backspace-string-compare/backspace-string-compare.java
+++ backspace-string-compare/backspace-string-compare.java
@@ -1,56 +1,46 @@
 class Solution {
-    public boolean backspaceCompare(String S, String T) {
-     
-        int sh = S.length();
-        int th = T.length();
+    public boolean backspaceCompare(String s, String t) {
         
-        int skS=0, skT=0;
         
-    int i = sh-1, j = th-1;
+        int l1 = s.length()-1, l2 = t.length()-1, skipS=0, skipT=0;
         
-        while(i >=0 || j >=0 ){
+        while(l1 >=0 || l2>=0){
             
+          while(l1 >=0 ){
+              
+              if(s.charAt(l1)== '#'){
+                  skipS++;
+                  l1--;
+              }else if(skipS > 0){
+                  skipS--; l1--;
+              }else{
+                  break;
+              }
+          }
             
-    
-        while(i >=0){
-            
-            if(S.charAt(i) == '#'){
-                skS++; i--;
-            
-            }else if(skS > 0){
-                skS --;
-                i--;
-            }else
-                break;
-             
-        }
-         while(j >=0){
-            
-            if(T.charAt(j) == '#'){
-                skT++; j--;
-            
-            }else if(skT > 0){
-                skT --;
-                j--;
-            }else
-                break;
-             
-        }
-            
-            
-        if(i>=0 && j>=0 && S.charAt(i)!=T.charAt(j)){
-            return false;
-        }
-
-        if(i >=0 != j>=0) 
-            return false;
-          
-         i--; j--;   
+             while(l2 >=0 ){
+              
+              if(t.charAt(l2)== '#'){
+                  skipT++;
+                  l2--;
+              }else if(skipT > 0){
+                  skipT--; l2--;
+              }else{
+                  break;
+              }
+          }
             
             
             
+            if(l1 >=0 && l2 >=0  && s.charAt(l1) != t.charAt(l2)){
+                return false;
+            }
+            
+            if((l1 >=0 )!=( l2 >=0)){
+                return false;
+            }
+            l1--; l2--;
         }
         return true;
-        
     }
 }
