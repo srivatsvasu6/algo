4dc7ad5e193f3e3ec4eca77789f3919be8dfc2db
Srivatsan Vasudevan <81347543+srivatsvasu6@users.noreply.github.com>
2021-08-28 20:49:04 -0500

    Time: 16 ms (5.31%), Space: 46.3 MB (6.72%) - LeetHub

--- subtree-of-another-tree/subtree-of-another-tree.java
+++ subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,51 +14,23 @@
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        
-        if(root == null && subRoot == null)
-            return true;
-          if(root == null || subRoot == null)
-            return false;
-        
-        
-      
-        Queue<TreeNode> q = new LinkedList<>();
-        
-        q.offer(root);
-        
-        while(!q.isEmpty()){
-            
-            for(int i = q.size(); i> 0; i--){
-                
-                TreeNode node = q.poll();
-                
-                if(isSameTree(node,subRoot)){
-                    return true;
-                }
-                
-                if(node.left!=null )
-                    q.offer(node.left);
-                
-                if(node.right!=null)
-                    q.offer(node.right);
-                
-                
-            }
-        }
-        
-        return false;
-        
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return serialize(s).contains(serialize(t)); // Java uses a naive contains algorithm so to ensure linear time, 
+                                                // replace with KMP algorithm
     }
-    
-    public boolean isSameTree(TreeNode t1, TreeNode t2){
         
-        if(t1 == null && t2 == null){
-            return true;
-        }else    if(t1 == null || t2 == null){
-            return false;
-        }
-        
-        return t1.val == t2.val && isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
-    }
+
+
+public String serialize(TreeNode root) {
+    StringBuilder res = new StringBuilder();
+    serialize(root, res);
+    return res.toString();
+}
+
+private void serialize(TreeNode cur, StringBuilder res) {
+    if (cur == null) {res.append(",#"); return;}
+    res.append("," + cur.val);
+    serialize(cur.left, res);
+    serialize(cur.right, res);
+}
 }
